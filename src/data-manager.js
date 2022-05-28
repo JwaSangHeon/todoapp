57bd9c8b3e31fc81a4de3cf0965f963abba64d12
JwaSangHeon <whk4051@naver.com>
2022-05-28 20:16:40 +0900

    수정

--- src/data-manager.js
+++ src/data-manager.js
@@ -2,9 +2,12 @@ const savedData = (data) => {
   localStorage.setItem("todos", JSON.stringify(data));
 };
 
-const loadData = () => {
+const loadData = (callback) => {
   const dataString = localStorage.getItem("todos") || "[]";
   return JSON.parse(dataString);
+  // fetch("http://localhost:3000/todos")
+  //   .then((res) => res.json())
+  //   .then((data) => callback(data));
 };
 
 export { savedData, loadData };
